8cb2579c7e5d1792fa1a0b5f8bba09fde570dfe1
Mahesh <50948582+Mrmyak@users.noreply.github.com>
2022-07-03 12:19:38 +0545

    /notice , /login

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
-import './App.css';
-import Sidebar from "./components/Sidebar";
-import Feed from "./components/Feed";
-import Widgets from "./components/Widgets";
-import Login from "./pages/login";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Navbar from './components/navbar/Navbar';
-
-
-
+import "./App.css"
+import Sidebar from "./components/Sidebar"
+import Feed from "./components/Feed"
+import Widgets from "./components/Widgets"
+import Login from "./pages/login"
+import Notice from "./components/Notice"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import Navbar from "./components/navbar/Navbar"
 
 function App() {
-  return (
-    <Router>
-      <div className="app">
-        <Sidebar />
-        <Navbar />
-        <Routes>
-          <Route exact path="/" element={<Feed />} />
-
-        </Routes>
-
-      </div>
-    </Router>
-
-  );
+    return (
+        <Router>
+            <div className="app">
+                <Sidebar />
+                <Navbar />
+                <Routes>
+                    <Route exact path="/" element={<Feed />} />
+                    <Route path="/notice" element={<Notice />} />
+                    
+                    <Route path="/login" element={<Login />} />
+                </Routes>
+            </div>
+        </Router>
+    )
 }
 
-export default App;
+export default App
